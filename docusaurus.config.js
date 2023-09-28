daa0ca7a3789a9dfd6e5d844337ba372e0b0e03e
Hatton <hattonjohn@gmail.com>
2023-09-28 17:47:56 -0600

    chore: Get ready for docu-notion v0.16 by using `notion-styles.css`
    Because I am committing this file, it will work on the existing docu-notion, but then when we upgrade the dependency, it will get automatically updated by docu-notion.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,9 +30,9 @@ const config = {
         blog: false,
         theme: {
           customCss: [
-            require.resolve("./src/css/custom.css"),
+            require.resolve("./css/docu-notion-styles.css"),
             require.resolve(
-              "./src/css/gifplayer.css"
+              "./css/gifplayer.css"
               //"./node_modules/react-gif-player/dist/gifplayer.css" // this gave a big red compile warning which is seaming unrelated "  Replace Autoprefixer browsers option to Browserslist config..."
             ),
           ],
